9c9b4e3420d836197363a8102785134ce67ebcc7
reniuszka <renia.diurczak@gmail.com>
2023-04-09 21:56:00 +0200

    pic edited

--- app/components/MyProfilePic.tsx
+++ app/components/MyProfilePic.tsx
@@ -6,8 +6,8 @@ const MyProfilePic = (props: Props) => {
   return (
     <section className="w-full mx-auto">
       <Image
-        className="border-4 border-slate-300 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
-        src="/images/climbing.jpeg"
+        className="border-4 border-slate-300 drop-shadow-xl shadow-black rounded-full mx-auto my-16"
+        src="/images/renata-diurczak.jpeg"
         width={200}
         height={200}
         alt="Renata Diurczak"
